3761b1af7cad5146930ce2e7a926ffed64e49b27
RealmEco <biglongnew@naver.com>
2024-12-24 23:08:56 +0900

    a

---
+++ Assets/Scripts/Practice.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Practice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("Normal");
+        //Debug.LogWarning("Warning");
+        //Debug.LogError("Error");
+
+        for(int i = 0; i < 100; i++) {
+            Debug.Log("Same Log");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
